04373f6d8ac2417c976a535dfed66af997597e40
Sergey <sergeyshishkalov@gmail.com>
2016-02-08 01:42:31 +0300

    Add tests for some container components

---
+++ src/containers/__tests__/CurrentPage.js
@@ -0,0 +1,41 @@
+import React, {Component} from 'react'
+import ReactDOM from 'react-dom'
+import {Simulate} from 'react-addons-test-utils'
+import renderer from 'utils/tests/renderer'
+
+import CurrentPageConnected, {CurrentPage} from '../CurrentPage'
+import PageLoader from 'layouts/PageLoader'
+
+describe('CurrentPage', () => {
+  let component
+  let render
+
+  before(() => {
+    render = renderer(CurrentPage)
+  })
+
+  context('current page is not yet specified', () => {
+    it('renders page loader', () => {
+      component = render({currentPageId: null})
+      expect(component).to.equalJSX(<PageLoader />)
+    })
+  })
+
+  context('current page is specified', () => {
+    it('renders associated page container', () => {
+      const Home = class extends Component { render() { return(<div></div>) } }
+      const Login = class extends Component { render() { return(<div></div>) } }
+      CurrentPageConnected.__Rewire__('Pages', {Home, Login})
+
+      component = render({currentPageId: 'home'})
+      expect(component).to.equalJSX(<Home />)
+      expect(component).not.to.equalJSX(<Login />)
+
+      component = render({currentPageId: 'login'})
+      expect(component).not.to.equalJSX(<Home />)
+      expect(component).to.equalJSX(<Login />)
+
+      CurrentPageConnected.__ResetDependency__('Pages')
+    })
+  })
+})
---
+++ src/containers/pages/Login/__tests__/Login.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import {Simulate} from 'react-addons-test-utils'
+import renderer from 'utils/tests/renderer'
+import domRenderer from 'utils/tests/domRenderer'
+
+import LoginConnected, {Login} from '../index'
+import Page from 'layouts/Page'
+import Form from 'forms/Form'
+
+describe('Login', () => {
+  let component
+  let render
+  const TextBox = (props) => { return <div></div> }
+  const Button = (props) => { return <div></div> }
+
+  before(() => {
+    render = domRenderer(Login)
+    LoginConnected.__Rewire__('TextBox', TextBox)
+    LoginConnected.__Rewire__('Button', Button)
+  })
+
+  after(() => {
+    LoginConnected.__ResetDependency__('TextBox')
+    LoginConnected.__ResetDependency__('Button')
+  })
+
+  it('renders a login form with submit handler', () => {
+    const submit = sinon.spy
+    component = render({fields: {username: {}, password: {}}, submitting: false, submit})
+
+    expect(component.render()).to.eql(
+      <Page fixed photo='amsterdam'>
+        <Form onSubmit={submit}>
+          <Form.Fields>
+            <TextBox autoFocus label='Email' />
+            <TextBox password label='Password' />
+          </Form.Fields>
+          <Form.Actions centered>
+            <Button submit isLoading={false}>Log In</Button>
+          </Form.Actions>
+        </Form>
+      </Page>
+    )
+  })
+})
