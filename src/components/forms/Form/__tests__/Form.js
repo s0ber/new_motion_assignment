389ee96c8863ca6d54a4c28fd024b6f46a2d424d
Sergey <sergeyshishkalov@gmail.com>
2016-02-07 03:59:08 +0300

    Improve Form tests

--- src/components/forms/Form/__tests__/Form.js
+++ src/components/forms/Form/__tests__/Form.js
@@ -1,36 +1,25 @@
-import {Simulate} from 'react-addons-test-utils'
 import * as TestUtils from 'react-shallow-testutils'
 import renderer from 'utils/tests/renderer'
-import domRenderer from 'utils/tests/domRenderer'
 
 import Form from '../Form'
 
 describe('Form', () => {
   let component
-  let render
+  const render = renderer(Form)
 
   describe('rendering', () => {
-    before(() => {
-      render = renderer(Form)
-    })
-
     it('renders form tag', () => {
       component = render({onSubmit: sinon.spy()})
-      expect(TestUtils.findWithType(component, 'form').type).to.eq('form')
+      expect(TestUtils.findWithType(component, 'form')).not.to.be.null
     })
   })
 
   describe('events handling', () => {
-    before(() => {
-      render = domRenderer(Form)
-    })
-
     it('handles submit with props.onSubmit function', () => {
       const handleSubmit = sinon.spy()
       component = render({onSubmit: handleSubmit})
 
-      Simulate.submit(component.node)
-      expect(handleSubmit).to.be.calledOnce
+      expect(TestUtils.findWithType(component, 'form').props.onSubmit).to.eq(handleSubmit)
     })
   })
 })
