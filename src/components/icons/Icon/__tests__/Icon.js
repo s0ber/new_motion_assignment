6df06e7d56be0c412ac62b9656cd0a7bfb45efdc
Sergey <sergeyshishkalov@gmail.com>
2016-02-07 15:10:47 +0300

    Add tests for icon

---
+++ src/components/icons/Icon/__tests__/Icon.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import renderer from 'utils/tests/renderer'
+
+import Icon from '../Icon'
+
+describe('Icon', () => {
+  let component
+  const render = renderer(Icon)
+
+  it('renders icon with font awesome class when props.fa is provided', () => {
+    component = render({fa: 'chevron-right'})
+    expect(component).to.equalJSX(<i className='fa fa-chevron-right' />)
+  })
+})
